8dbc0196b63672f71636f5767bb927128e07f792
flery <flery@gmx.net>
2012-06-12 23:17:04 +0200

    added initial header parsing of modbus telegrams

--- modbus.js
+++ modbus.js
@@ -1,13 +1,34 @@
 var net = require('net');
 
-var connection = net.createConnection(502);
+var Request = {};
+var Response = {};
 
-connection.on('connect', function() {
-	console.log('connected');
-	connection.write('Hello');
+var ReadMultipleRegisters = {};
+ReadMultipleRegisters.parse = function(buffer) {
+};
+
+var server = net.createServer(function(socket) { //'connection' listener
+  console.log('server connected');
+  socket.on('end', function() {
+    console.log('server disconnected');
+  });
+  
+    socket.on('data', function(data) {
+		console.log(data);
+        socket.write(data);
+		
+		console.log(data.length);
+		var telegram = {};
+		telegram.transactionIdentifier = data.readInt16BE(0);
+		telegram.protocolIdentifier =  data.readInt16BE(2);
+		telegram.length =  data.readInt16BE(4);
+		telegram.unitIdentifier = data.readInt8(6);
+		telegram.functionCode = data.readInt8(7);
+		
+		console.log(telegram);
+    });
 });
 
-connection.on('close', function() {
-	console.log('closed');
-	connection.end();
+server.listen(502, function() { //'listening' listener
+  console.log('server bound');
 });
