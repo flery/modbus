1a2fbf7f37481f5a05b23c83fef067770ffbf389
flery <flery@gmx.net>
2012-06-17 01:20:24 +0200

    implemented basic fc3 handling implemented exception handling for illegal fc

--- modbus.js
+++ modbus.js
@@ -15,7 +15,6 @@ var server = net.createServer(function(socket) { //'connection' listener
   
     socket.on('data', function(data) {
 		console.log(data);
-        socket.write(data);
 		
 		console.log(data.length);
 		var telegram = {};
@@ -25,7 +24,53 @@ var server = net.createServer(function(socket) { //'connection' listener
 		telegram.unitIdentifier = data.readInt8(6);
 		telegram.functionCode = data.readInt8(7);
 		
+		switch(telegram.functionCode)
+		{
+
+		case 3:
+			console.log("recieved FC3");
+			telegram.offset = data.readInt16BE(8);
+			telegram.size = data.readInt16BE(10);
+			
+			var rsp = new Buffer(telegram.size*2+9); // payload + header
+			
+			// mbtcp header
+			rsp.writeInt16BE(telegram.transactionIdentifier,0);
+			rsp.writeInt16BE(telegram.protocolIdentifier,2);
+			rsp.writeInt16BE(telegram.size*2+3,4);
+			
+			rsp.writeInt8(telegram.unitIdentifier,6);
+			rsp.writeInt8(telegram.functionCode,7);	
+			rsp.writeInt8(telegram.size*2,8); // 2*wordcount as data payload size
+			
+			for (var i = 0; i < telegram.size; i++)
+			{
+				rsp.writeInt16BE(i,9+(i*2));
+			}
+			console.log(rsp);
+			socket.write(rsp);
+			break;
+		
+		// raise illegal function exception
+		default:
+			console.log("default");
+			var rsp = new Buffer(9);
+			
+			// mbtcp header
+			rsp.writeInt16BE(telegram.transactionIdentifier,0);
+			rsp.writeInt16BE(telegram.protocolIdentifier,2);
+			rsp.writeInt16BE(3,4); // 3 bytes payload
+			
+			rsp.writeInt8(telegram.unitIdentifier,6);
+			rsp[7] = 0x80 | telegram.functionCode; // exception rsp function code
+			rsp[8] = 0x01; // exception code
+			
+			console.log(rsp);
+			socket.write(rsp);
+			
+		}
 		console.log(telegram);
+		
     });
 });
 
